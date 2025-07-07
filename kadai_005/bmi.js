da6913b1534abc4de0a7fad0e21e762518bb6ac0
yym2525 <jk91yk25@outlook.jp>
2025-07-07 21:04:21 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言
+let weight;
+let height;
+
+// 値の代入
+weight = Number('68');
+height = Number('1.7');
+
+console.log(height * height);
+
+let bmi = weight / (height * height);
+
+console.log(bmi);
